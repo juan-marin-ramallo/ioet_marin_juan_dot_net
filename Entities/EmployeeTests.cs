3b1e5d6d50c36d6871326e32d9186f13c5a744b3
Juan Marin <jmarin@espol.edu.ec>
2022-06-02 16:13:01 -0500

    Unit Test developed

---
+++ Entities/EmployeeTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ioet_marin_juan_dot_net.entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ioet_marin_juan_dot_net.entities.Tests
+{
+    [TestClass()]
+    public class EmployeeTests
+    {
+        [TestMethod()]
+        public void GetEmployeeInfoTest()
+        {
+            string line = "RENE=MO10:00-12:00,TU10:00-12:00,TH01:00-03:00,SA14:00-18:00,SU20:00-21:00";
+            Employee? employee_job_schedule_entity = Employee.GetEmployeeInfo(line);
+            Assert.AreEqual(employee_job_schedule_entity!.Name, "RENE");
+        }
+    }
+}
---
+++ Utils/SalaryCalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ioet_marin_juan_dot_net.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ioet_marin_juan_dot_net.Utils.Tests
+{
+    [TestClass()]
+    public class SalaryCalculatorTests
+    {
+        [TestMethod()]
+        public void CalculatePayByEmployeeTest1()
+        {
+            string line = "RENE=MO10:00-12:00,TU10:00-12:00,TH01:00-03:00,SA14:00-18:00,SU20:00-21:00";
+            double total_to_pay = SalaryCalculator.CalculatePayByEmployee(line);
+            Assert.AreEqual(total_to_pay, 215);
+        }
+    }
+}
